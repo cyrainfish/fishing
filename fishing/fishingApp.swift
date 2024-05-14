13c470d1622d49b83d5debdaafcf5e382ea61a40
ycao <ycao@freewheel.tv>
2024-05-14 18:46:25 +0800

    Initial Commit

---
+++ fishing/fishingApp.swift
@@ -0,0 +1,17 @@
+//
+//  fishingApp.swift
+//  fishing
+//
+//  Created by Cao, Yu on 2024/5/14.
+//
+
+import SwiftUI
+
+@main
+struct fishingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
